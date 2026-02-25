19036cb8283647060f1e9b24ccfbf9cd4e7306a2
Amighi Thomas <thomas.amighi@hotmail.com>
2026-02-25 13:56:08 +0100

    dev clients

---
+++ clients/admin/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="bg-amber-50">Hello admin</div>;
+}
+
+export default App;
---
+++ clients/site/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="bg-red-50">Hello site</div>;
+}
+
+export default App;
