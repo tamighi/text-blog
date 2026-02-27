0834803817476c6e04bc6be45029461b298ab5cc
Amighi Thomas <thomas.amighi@hotmail.com>
2026-02-27 18:13:49 +0100

    feat: admin api

--- clients/admin/src/pages/PostPage.tsx
+++ clients/admin/src/pages/PostPage.tsx
@@ -1,4 +1,15 @@
+import { postService } from "@shared/api/postService";
+import React from "react";
+
 const PostPage = () => {
+  React.useEffect(() => {
+    const getPosts = async () => {
+      const posts = await postService.list();
+      console.log(posts);
+    };
+
+    getPosts();
+  }, []);
   return <div>PostPage</div>;
 };
 
