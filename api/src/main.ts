80b36533779f93ae04c262b63de981665bf67caf
Amighi Thomas <thomas.amighi@hotmail.com>
2026-02-28 11:13:50 +0100

    fix: shutdown hook prod_api podman

--- api/src/main.ts
+++ api/src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("/docs", app, document);
 
+  app.enableShutdownHooks();
+
   await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
 }
 bootstrap();
--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
     super({ adapter });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
